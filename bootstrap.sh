5317efcaf7dc90583909001c0ab3288f407664c8
Kim Desrosiers <kimdesro@gmail.com>
2014-03-11 18:08:23 -0400

    add of bundles and pathogen script

---
+++ bootstrap.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+if [ -d "~/.vim" ]; then
+    # Control will enter here if $DIRECTORY exists.
+    mv ~/.vim ~/.vim_old
+fi
+
+
+if [ -d "~/vimfiles" ]; then
+    # Control will enter here if $DIRECTORY exists.
+    mv ~/vimfile ~/vimfiles_old
+fi
+
+if [ -f "~/.vimrc" ]; then
+    mv ~/.vimrc ~/.vimrc_old
+fi
+
+#Installing the base directory of the vim files
+cd ~/
+git clone https://github.com/kimond/vimfiles.git
+
+ln -s vimfiles .vim
+ln -s vimfiles/.vimrc .vimrc
+
+cd vimfiles
+git submodule update --init
+
+cd bundle/jedi-vim
+
+git submodule update --init
